4ce57c8eef6bfaffdaebc16e0cf6d2bc9ad9bc1a
robbie <qunfengqiu@gmail.com>
2018-11-28 19:25:07 +0800

    support redis cache

--- src/main/java/com/licenseservice/Client/OrganizationRestTemplateClient.java
+++ src/main/java/com/licenseservice/Client/OrganizationRestTemplateClient.java
@@ -2,6 +2,9 @@ package com.licenseservice.Client;
 
 import com.licenseservice.Model.License;
 import com.licenseservice.Model.Organization;
+import com.licenseservice.repository.OrganizationRedisRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +14,49 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class OrganizationRestTemplateClient {
 
+    private final static Logger logger = LoggerFactory.getLogger(OrganizationRestTemplateClient.class);
+
     @Autowired
     private RestTemplate restTemplate = null;
 
+    @Autowired
+    OrganizationRedisRepository redisRepository;
+
+    private Organization checkRedisCache(Long orgId){
+        try {
+            return redisRepository.findOrganization(orgId);
+        } catch (Exception ex) {
+            logger.error("error occurs when retrieve redis cache");
+            return null;
+        }
+    }
+
+    private void cacheOrgObject(Organization organization){
+        try {
+            redisRepository.saveOrganization(organization);
+        } catch (Exception ex){
+            logger.error("error occurs when saving redis cache");
+        }
+    }
+
+
     public Organization getOrganization(Long organizationId) {
+
+        Organization organization = checkRedisCache(organizationId);
+        if (organization != null){
+            logger.info("retrieve data from redis cache successfully");
+            return organization;
+        }
+
         ResponseEntity<Organization> responseEntity = restTemplate.exchange(
                 "http://organizationservice//v1/organizations/{organizationId}",
                 HttpMethod.GET, null, Organization.class, organizationId);
 
-        return responseEntity.getBody();
+        organization = responseEntity.getBody();
+        if (organization != null){
+            cacheOrgObject(responseEntity.getBody());
+        }
+
+        return organization;
     }
 }
--- src/main/java/com/licenseservice/Config/ServiceConfig.java
+++ src/main/java/com/licenseservice/Config/ServiceConfig.java
@@ -13,4 +13,31 @@ public class ServiceConfig {
 		return exampleProperty;
 	}
 
+    @Value("${redis.server}")
+    private String redisServer="";
+
+    @Value("${redis.port}")
+    private String redisPort="";
+
+    public void setExampleProperty(String exampleProperty) {
+        this.exampleProperty = exampleProperty;
+    }
+
+    public String getRedisServer() {
+        return redisServer;
+    }
+
+    public void setRedisServer(String redisServer) {
+        this.redisServer = redisServer;
+    }
+
+    public Integer getRedisPort() {
+        return new Integer(redisPort).intValue();
+    }
+
+    public void setRedisPort(String redisPort) {
+        this.redisPort = redisPort;
+    }
+
+
 }
